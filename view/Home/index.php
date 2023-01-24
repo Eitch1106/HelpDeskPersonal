e1ab212ac980d54559c2e2e599d58a6d4038fedd
Eitch1106 <99038337+Eitch1106@users.noreply.github.com>
2023-01-24 13:00:02 -0600

    More changes about views

--- view/Home/index.php
+++ view/Home/index.php
@@ -1,1 +1,33 @@
-<h1>Home</h1>
+<?php
+require_once("../../config/conexion.php");
+if(isset($_SESSION["usu_id"])){
+?>
+<!DOCTYPE html>
+<html>
+    <?php require_once("../MainHead/head.php");?>
+    <title>HelpDesk Personal</>::Home</title>
+</head>
+<body class="with-side-menu">
+
+    <?php require_once("../MainHeader/header.php");?>
+
+	<div class="mobile-menu-left-overlay"></div>
+    
+	<?php require_once("../MainNav/nav.php");?>
+<!-- Esto va a cambiar -->
+	<div class="page-content">
+		<div class="container-fluid">
+			Blank page.
+		</div><!--.container-fluid-->
+	</div><!--.page-content-->
+
+    <?php require_once("../MainJs/js.php");?>
+	<script type="text/javascript" src="home.js"></script>
+</body>
+</html>
+<?php
+}
+else{
+	header("Location:".Conectar::ruta()."index.php");
+}
+?>
---
+++ view/MainHeader/header.php
@@ -0,0 +1,67 @@
+<header class="site-header">
+	    <div class="container-fluid">
+	
+	        <a href="#" class="site-logo">
+	            <img class="hidden-md-down" src="../../public/img/logo-2.png" alt="">
+	            <img class="hidden-lg-up" src="../../public/img/logo-2-mob.png" alt="">
+	        </a>
+	
+	        <button id="show-hide-sidebar-toggle" class="show-hide-sidebar">
+	            <span>toggle menu</span>
+	        </button>
+	
+	        <button class="hamburger hamburger--htla">
+	            <span>toggle menu</span>
+	        </button>
+            
+	        <div class="site-header-content">
+	            <div class="site-header-content-in">
+	                <div class="site-header-shown">
+	                    <div class="dropdown user-menu">
+	                        <button class="dropdown-toggle" id="dd-user-menu" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	                            <img src="../../public/img/avatar-2-64.png" alt="">
+	                        </button>
+	                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
+	                            <a class="dropdown-item" href="../Perfil/"><span class="font-icon glyphicon glyphicon-user"></span>Profile</a>
+	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-cog"></span>Settings</a>
+	                            <div class="dropdown-divider"></div>
+	                            <a class="dropdown-item" href="../Logout/logout.php"><span class="font-icon glyphicon glyphicon-log-out"></span>Logout</a>
+	                        </div>
+	                    </div>
+	                </div><!--.site-header-shown-->
+	
+	                <div class="mobile-menu-right-overlay"></div>
+	                <div class="site-header-collapsed">
+	                    <div class="site-header-collapsed-in">
+	                        
+	                        <div class="dropdown dropdown-typical">
+	                            <a class="dropdown-toggle no-arr"" href="#" >
+	                                <span class="font-icon font-icon-home"></span>
+	                                <span class="lbl">Home</span>
+	                            </a>
+	
+	                            
+	                        </div>
+
+							<input type="hidden" id="user_idx" value="<?php echo$_SESSION["usu_id"] ?>"> <!-- Id global from user -->
+
+	                        <div class="dropdown dropdown-typical">
+	                            <a class="dropdown-toggle no-arr"" href="#">
+	                                <span class="font-icon font-icon-user"></span>
+	                                <span class="lblcontactonomx"><?php echo $_SESSION["usu_nom"]?> <?php echo $_SESSION["usu_ape"]?></span>
+	                            </a>
+	
+	                        </div>
+	                        <!-- <div class="dropdown dropdown-typical">
+	                            <a href="#" class="dropdown-toggle no-arr">
+	                                <span class="font-icon font-icon-contacts"></span>
+	                                <span class="lbl">Contacts</span>
+	                            </a>
+	                        </div> -->
+	
+	                    </div><!--.site-header-collapsed-in-->
+	                </div><!--.site-header-collapsed-->
+	            </div><!--site-header-content-in-->
+	        </div><!--.site-header-content-->
+	    </div><!--.container-fluid-->
+	</header><!--.site-header-->
